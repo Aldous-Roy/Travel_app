c388b0d0aa64aa50bf7cf7df746c521fcdcb1a64
Aldous Roy <aldousroy007@gmail.com>
2025-12-31 21:54:04 +0530

    Add main components (Hero, Features, Guide, Camp, GetApp) to Home page and update Button variant type

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import Hero from './Hero'
+import Features from './Features'
+import Guide from './Guide'
+import Camp from './Camp'
+import GetApp from './GetApp' 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Hero />
+      <Features />
+      <Guide />
+      <Camp />
+      <GetApp />
+    </>
   )
 }
 
