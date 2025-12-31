8ccb886a197eb1b57c9d91022adbadcc46918452
Aldous Roy <aldousroy007@gmail.com>
2025-12-31 20:58:23 +0530

    Add YouTube SVG icon and configure Tailwind CSS with custom theme settings
    - Added a new YouTube SVG icon to the public directory.
    - Created a Tailwind CSS configuration file with custom colors, background images, screen sizes, max width, and border radius settings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
-export default page
+export default Home
---
+++ components/Button.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <div>
+      <button
+        type={type}
+        className={`flexCenter gap-3 ${variant} rounded-full border `}
+      >
+        {icon && (
+          <Image
+            src={icon}
+            alt="button icon"
+            width={20}
+            height={20}
+            className="w-5 h-5"
+          />
+        )}
+        <label className="bold-16 whitespace-nowrap">{title}</label>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
