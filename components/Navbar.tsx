b38292a50afaa93d5065953821c654c87c5a7cc2
Aldous Roy <aldousroy007@gmail.com>
2026-01-03 22:45:15 +0530

    feat: Add mobile navigation with smooth scrolling, update section IDs, and adjust navigation links to use anchor scrolling.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
+"use client"
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -28,12 +36,34 @@ const Navbar = () => {
       </div>
 
       <Image 
-        src="menu.svg"
+        src={isMenuOpen ? "close.svg" : "menu.svg"}
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
       />
+
+      {isMenuOpen && (
+        <ul className="absolute top-full left-0 w-full bg-white flex flex-col items-center gap-6 py-10 lg:hidden shadow-lg border-t border-gray-100">
+          {NAV_LINKS.map((link) => (
+            <Link 
+              href={link.href} 
+              key={link.key} 
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {link.label}
+            </Link>
+          ))}
+           <Button 
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+          />
+        </ul>
+      )}
     </nav>
   )
 }
